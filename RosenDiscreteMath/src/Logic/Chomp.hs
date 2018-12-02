aca15f6e29f635b5fb87bb9245f867fc5d31c613
vincenteof <vincenteof@qq.com>
2018-12-02 11:42:28 +0800

    finish Chomp.hs

--- RosenDiscreteMath/src/Logic/Chomp.hs
+++ RosenDiscreteMath/src/Logic/Chomp.hs
@@ -1,3 +1,5 @@
+-- {-# LANGUAGE MultiWayIf #-}
+
 module Logic.Chomp where
 
 import Control.Monad.State
@@ -10,20 +12,50 @@ type Moves = [Position]
 
 poisoned = (0, 0)
 
-
 -- determine whether a move is illegal
-illegalMove :: Moves -> Position -> Bool
-illegalMove moves pos = foldr judge True moves
-  where judge cur acc = not (cur `inRange` pos) && acc
+legalMove :: Int -> Int -> Moves -> Position -> Bool
+legalMove m n moves pos = inBound m n pos && foldr judge True moves
+  where judge prevPos acc = not (pos `beCovered` prevPos) && acc
+  
+inBound :: Int -> Int -> Position -> Bool
+inBound m n (curX, curY) = m > curX && n > curY
 
-inRange :: Position -> Position -> Bool
-inRange (curX, curY) (x, y) = curX >= x && curY >= y
+beCovered :: Position -> Position -> Bool
+beCovered (x, y) (prevX, prevY) = x >= prevX && y >= prevY
+
+poisonedMove :: Position -> Bool
+poisonedMove cur = cur == poisoned 
 
 -- make some move and change global state
-data GameResult = InGoing | End
+data GameResult = InGoing | AWin | BWin deriving Show
+data Player = A | B deriving (Show, Read)
+type GameStatus = (Moves, Player)
 
-makeMove :: StateT Moves IO GameResult
-makeMove = undefined
+chomp :: Int -> Int -> StateT GameStatus IO GameResult
+chomp m n = do
+  line <- lift getLine
+  let pos = read line :: Position
+  (moves, player) <- get
+  if legalMove m n moves pos
+    then change m n pos moves player
+    else do
+      lift (putStrLn "Invalid Move!")
+      chomp m n
 
--- chomp :: Int -> Int -> IO ()
--- chomp m n = undefined
+change :: Int -> Int -> Position -> Moves -> Player -> StateT GameStatus IO GameResult
+change m n pos moves player = do
+  put (pos:moves, anotherPlayer player)  
+  if poisonedMove pos 
+    then return (whoWin player)
+    else chomp m n 
+
+-- helper function    
+anotherPlayer :: Player -> Player
+anotherPlayer A = B
+anotherPlayer B = A
+
+whoWin :: Player -> GameResult
+whoWin A = BWin
+whoWin B = AWin
+
+gameStart = runStateT (chomp 4 4) ([], A)
