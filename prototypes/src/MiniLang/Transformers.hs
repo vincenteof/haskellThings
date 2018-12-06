6f967d2ac53ccabe16c4304d0bbdbfa828d53ee3
vincenteof <vincenteof@qq.com>
2018-12-06 22:02:19 +0800

    eval2, add error info

--- prototypes/src/MiniLang/Transformers.hs
+++ prototypes/src/MiniLang/Transformers.hs
@@ -30,22 +30,31 @@ type Eval2 a = ExceptT String Identity a
 runEval2 :: Eval2 a -> Either String a
 runEval2 = runIdentity . runExceptT
 
-eval2a :: Env -> Exp -> Eval2 Value
+eval2 :: Env -> Exp -> Eval2 Value
 
-eval2a env (Lit num) = return $ IntVal num
+eval2 env (Lit num) = return $ IntVal num
 
-eval2a env (Var name) = return . fromJust . M.lookup name $ env
+-- eval2 env (Var name) = return . fromJust . M.lookup name $ env
+eval2 env (Var name) = case M.lookup name env of
+  Nothing -> throwError $ "unbounded variable: " ++ name
+  Just v -> return v
 
-eval2a env (Plus exp1 exp2) = do
-  IntVal num1 <- eval2a env exp1
-  IntVal num2 <- eval2a env exp2
-  return . IntVal $ num1 + num2
+eval2 env (Plus exp1 exp2) = do
+  v1 <- eval2 env exp1
+  v2 <- eval2 env exp2
+  case (v1, v2) of
+    (IntVal num1, IntVal num2) -> return . IntVal $ num1 + num2
+    _ -> throwError "type error in addition"
 
-eval2a env (Abs name exp) = return $ FunVal env name exp
+eval2 env (Abs name exp) = return $ FunVal env name exp
 
-eval2a env (App funcExp exp) = do
-  FunVal funcEnv name body <- eval2a env funcExp
-  val <- eval2a env exp
-  eval2a (M.insert name val funcEnv) body
+eval2 env (App funcExp exp) = do
+  v1 <- eval2 env funcExp
+  v2 <- eval2 env exp
+  case v1 of
+    FunVal funcEnv name body -> eval2 (M.insert name v2 funcEnv) body
+    _ -> throwError "type error in application"
+
 
 exampleExp = Lit 12 `Plus` App (Abs "x" (Var "x")) (Lit 4 `Plus` Lit 2)
+
