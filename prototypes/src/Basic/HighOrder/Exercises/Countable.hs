5ced30c1a04ba40f2f35994129cfcb53ab5d8bde
vincenteof <vincenteof@qq.com>
2019-01-21 23:04:52 +0800

    solving the problem using tricks

--- prototypes/src/Basic/HighOrder/Exercises/Countable.hs
+++ prototypes/src/Basic/HighOrder/Exercises/Countable.hs
@@ -1,8 +1,21 @@
 module Basic.HighOrder.Exercises.Countable where 
     
--- combine 2 infinite list    
+-- combine 2 infinite list, ignore error case    
 interLeave :: [a] -> [a] -> [a]
 interLeave xs ys = foldr (\(x, y) acc -> x : y: acc) [] (zip xs ys)
+
+-- all possible combinations
+
+-- interLeaveLists :: [[a]] -> [a]
+-- interLeaveLists = foldr1 interLeave
+-- allResults = interLeaveLists [[(x, y) | y <- [1..]] | x <- [1..]]
+
+combinationsInRange :: Integer -> [(Integer, Integer)]
+combinationsInRange n
+    | n < 2 = []
+    | otherwise = [(x, n - x) | x <- [1..n-1]]
+
+allCombinations = concat [combinationsInRange x | x <- [2..]]
 
 -- remove duplicate
 removeOne :: Eq a => a -> [a] -> [a]
