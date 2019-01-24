b0649691090dadaab7d53a25b6dd2447537ff69b
vincenteof <vincenteof@qq.com>
2019-01-24 21:25:16 +0800

    using hand-made monad

--- prototypes/src/Transformer/HandMade.hs
+++ prototypes/src/Transformer/HandMade.hs
@@ -3,6 +3,7 @@
 module Transformer.HandMade where 
  
 import Data.Text
+import qualified Data.Text.IO as T
 
 data LoginError = InvalidEmail 
     | NoSuchUser
@@ -14,7 +15,7 @@ getDomain email =
         [name, domain] -> Right domain
         _              -> Left InvalidEmail        
     
-        
+-- Combine `Either` and `IO`, and make it a monad                
 newtype EitherIO e a = EitherIO {
     runEitherIO :: IO (Either e a)
 }
@@ -27,4 +28,16 @@ instance Applicative (EitherIO e) where
     mf <*> ma = EitherIO $ pure (<*>) <*> rawF <*> rawA
         where rawF = runEitherIO mf
               rawA = runEitherIO ma  
+
+instance Monad (EitherIO e) where
+    return = pure
+    ma >>= f = EitherIO $ runEitherIO ma >>= either (return . Left) (runEitherIO . f)
+
+-- Use our hand-made monad    
+getToken :: EitherIO LoginError Text
+getToken = do
+    EitherIO (fmap Right (T.putStrLn "Enter email address:"))
+    input <- EitherIO (fmap Right T.getLine)
+    EitherIO (return (getDomain input))
+
 
